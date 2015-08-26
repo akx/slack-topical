ae2038e4f59d7557002c3d61711897f9c23d22f4
Aarni Koskela <akx@iki.fi>
2015-08-26 09:49:17 +0300

    More graceful jzing

--- jz.js
+++ jz.js
@@ -1,12 +1,14 @@
 const Promise = require("bluebird");
 const request = require("request");
 const merge = require("merge");
+const log = require("./log");
 
 export default function(url, options) {
     options = merge.recursive({
         url: url,
         headers: {"User-Agent": "Slack-Topical"}
     }, options || {});
+    log.d("jz", "Requesting %s", url);
     return new Promise(function(resolve, reject) {
         request(options, function(error, response, body) {
             if(error) return reject(error);
@@ -15,6 +17,7 @@ export default function(url, options) {
             } catch(error) {
                 return reject(error);
             }
+            if(response.statusCode >= 400) return reject(result);
             if(options.mutate) result = options.mutate(result);
             return resolve(result);
         });
